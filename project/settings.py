ef0822bdd8490e9df7f9937c586d1c1aa876da1a
NexusGk Softwares <nexusgksoftwares@gmail.com>
2024-11-06 16:01:47 +0300

    "Updated static files settings: changed STATIC_URL, added STATIC_ROOT and STATICFILES_DIRS"

--- project/settings.py
+++ project/settings.py
@@ -116,7 +116,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = '/static/'
+
+# The directory where static files will be collected
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Additional directories where Django will look for static files
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
