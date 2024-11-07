d59847d33d4184245c45f9bb15655b6acf930862
NexusGk Softwares <nexusgksoftwares@gmail.com>
2024-11-07 11:55:55 +0300

    "Updated URLs and settings files in application and project directories"

--- application/urls.py
+++ application/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),  # Root URL of the application
-    path('about/', views.about, name='about'),  # Example 'about' page
-    path('contact/', views.contact, name='contact'),  # Example 'about' page
-    path('base/', views.base, name='base'),  # Example 'about' page
+    path('about/', views.about, name='about'), 
+    path('contact/', views.contact, name='contact'), 
+    path('base/', views.base, name='base'), 
 
 ]
--- project/urls.py
+++ project/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('application/', include('application.urls')),  # Include your application's URLs
+    path('application/', include('application.urls')),  
 ]
