e975de3ef992646f447bd49e7422fb3c20742574
NexusGk Softwares <nexusgksoftwares@gmail.com>
2024-11-11 07:55:18 +0300

    final changes for week 5

--- application/forms.py
+++ application/forms.py
@@ -1,5 +1,3 @@
-
-
 from django import forms
 
 from application.models import Student
@@ -7,12 +5,13 @@ from application.models import Student
 
 class StudentForm(forms.ModelForm):
     class Meta:
-        model = Student 
+        model = Student
         fields = '__all__'
-        
+
         widgets = {
-            'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder':'enter your name'}),
-            'age': forms.TextInput(attrs={'class': 'form-control', 'placeholder':'enter your name'}),
-            'gender': forms.TextInput(attrs={'class': 'form-control', 'placeholder':'enter your name'}),
-            'course': forms.TextInput(attrs={'class': 'form-control', 'placeholder':'enter your name'}),
+            'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter your name'}),
+            'age': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Enter your age'}),
+            'gender': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter your gender'}),
+            'course': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter your course'}),
+
         }
