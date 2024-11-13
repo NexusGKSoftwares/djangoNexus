52b257e0cabb993ba9cb026fed7030293fb2ee21
NexusGKSoftwares <nexusgk3@gmail.com>
2024-11-13 16:44:00 +0300

    "Added profile_picture field to Student model"

--- application/models.py
+++ application/models.py
@@ -1,13 +1,12 @@
 from django.db import models
 
-# Create your models here.
 class Student(models.Model):
     name = models.CharField(max_length=20)
     admission = models.CharField(max_length=20)
     age = models.IntegerField()
     gender = models.CharField(max_length=20)
     course = models.CharField(max_length=20)
+    profile_picture = models.ImageField(upload_to='student_images/', blank=True, null=True)
 
     def __str__(self):
         return self.name
-
