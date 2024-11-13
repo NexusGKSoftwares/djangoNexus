05523d59de52e9d16b65f906a619a7a486e299af
NexusGKSoftwares <nexusgk3@gmail.com>
2024-11-13 16:45:48 +0300

    "Updated models.cpython-310.pyc and settings.cpython-310.pyc files; added import os statement and modified media configuration in settings.py"

--- project/settings.py
+++ project/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 from django.conf.global_settings import STATICFILES_DIRS
@@ -122,8 +123,9 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
-MEDIAL_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+# Media configuration
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
