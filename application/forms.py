13c7e766508ad31138ae75b83344764dc42309d5
NexusGKSoftwares <nexusgk3@gmail.com>
2024-11-14 16:52:16 +0300

    "Updated forms.py, added changes to about.html, and modified db.sqlite3"

--- application/forms.py
+++ application/forms.py
@@ -8,7 +8,7 @@ class StudentForm(forms.ModelForm):
 
         widgets = {
             'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter your name'}),
-            'admission': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter your admission number'}),
+            'adm': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter your admission number'}),
             'age': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Enter your age'}),
             'gender': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter your gender'}),
             'course': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter your course'}),
