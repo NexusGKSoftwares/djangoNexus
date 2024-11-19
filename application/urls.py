365a5f410290ef231931d47fdaf25c170aed4813
NexusGKSoftwares <nexusgk3@gmail.com>
2024-11-19 16:52:29 +0300

    "Added new URL pattern for course list in urls.py"

--- application/urls.py
+++ application/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     path('students/edit/<int:id>/', views.edit, name='edit'),
     path('delete/<int:student_id>/', views.delete_student, name='delete'),
     path('studentsapi/', views.studentsapi, name='studentsapi'),
+    path('courses/', views.course_list, name='course_list'),
     path('courses/add/', views.add_course, name='add_course'),
     path('students/<int:student_id>/assign_courses/', views.assign_course, name='assign_course'),
 ]
