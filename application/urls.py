705592ec9b9bb9b27c12bdf84ba3632c297e1cc4
NexusGKSoftwares <nexusgk3@gmail.com>
2024-11-19 17:04:45 +0300

    "Added new URL pattern for deleting courses"

--- application/urls.py
+++ application/urls.py
@@ -14,6 +14,7 @@ urlpatterns = [
     path('studentsapi/', views.studentsapi, name='studentsapi'),
     path('courses/', views.course_list, name='course_list'),
     path('courses/edit/<int:id>/', views.edit_course, name='edit_course'),
+    path('courses/delete/<int:id>/', views.delete_course, name='delete_course'),
     path('courses/add/', views.add_course, name='add_course'),
     path('students/<int:student_id>/assign_courses/', views.assign_course, name='assign_course'),
 ]
