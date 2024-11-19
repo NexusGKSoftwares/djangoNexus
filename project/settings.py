b9c639fbe7da65dae6491696b6d621b4169a24a0
NexusGKSoftwares <nexusgk3@gmail.com>
2024-11-19 14:55:19 +0300

    "Updated urls and settings files, added rest_framework to INSTALLED_APPS"

--- project/settings.py
+++ project/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'application'
+    'application',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
